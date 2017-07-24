65d1b29ba587cca7ce4aa72411ccb4f15a6f8759
11 <nayul1013@gmail.com>
2017-07-24 16:58:53 +0900

    최초 커밋

---
+++ src/main/java/com/kny/back/KnyBackApplication.java
@@ -0,0 +1,12 @@
+package com.kny.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnyBackApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kny/back/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.kny.back;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KnyBackApplication.class);
+	}
+
+}
